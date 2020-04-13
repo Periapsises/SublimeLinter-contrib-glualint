95edcf771d6e72c91589e371e59bb06116c225f8
Periapsises <45960263+Periapsises@users.noreply.github.com>
2020-04-13 16:02:09 -0400

    Fix regex for Errors

--- linter.py
+++ linter.py
@@ -3,7 +3,7 @@ from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLi
 
 class GLua(Linter):
     cmd = 'glualint ${file} ${args}'
-    regex = r'.+\.lua: \[?(?P<error>Error)?(?P<warning>Warning)\] line (?P<line>\d+), column (?P<col>\d+) - line \d+, column \d+: (?P<message>.+)'
+    regex = r'.+\.lua: \[(?P<error>Error)?(?P<warning>Warning)?\] line (?P<line>\d+), column (?P<col>\d+) - line \d+, column \d+: (?P<message>.+)'
     multiline = True
     defaults = {
         'selector': 'source.lua'
